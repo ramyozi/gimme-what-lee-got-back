a3d5bb624d2860196b56f4a2032ca3cb22ca4587
Ramzi <ramzibenmansour.pro@gmail.com>
2025-09-10 19:42:25 +0200

    feat(users/model): Add roles (list) field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Class roles
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+# Custom user model with roles
+class CustomUser(AbstractUser):
+    roles = models.ManyToManyField(roles, blank=True)
--- users/serializers.py
+++ users/serializers.py
@@ -1,22 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import CustomUser
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
+        model = CustomUser
+        fields = ('username', 'password', 'email', 'roles')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create (self, validated_data):
+        roles = validated_data.pop('roles', [])
         user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
-            email=validated_data['email']
+            email=validated_data['email'],
         )
+
+        user.roles.set(roles)
         return user
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'roles')
