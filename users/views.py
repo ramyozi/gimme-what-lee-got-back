33896190146a3c15bc7dd4f6814774577bd72b80
Ramzi <ramzibenmansour.pro@gmail.com>
2025-09-10 18:45:35 +0200

    feat(users): Implement serializers for sign-in and login
    Casser le code (views - serializers)

--- users/views.py
+++ users/views.py
@@ -1,19 +1,7 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
-
-# User registration serializer
-class RegisterSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    qset = User.objects.all()
     serializer_class = RegisterSerializer
