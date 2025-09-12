83f5dea39091f71471f3b9c831369bdf9bc203e6
ramzi <ramzi.benmansour@artdigit.fr>
2025-09-13 00:55:58 +0200

    feat(serializers): nest relevant related models in catalog serializers

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+from users.models import CustomUser
 from .models import Category, Item, UserInteraction
+from rest_framework import serializers
+from users.serializers import CustomUserSerializer
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,11 +9,16 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Item
         fields = '__all__'
 
 class UserInteractionSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+    item = ItemSerializer(read_only=True)
+
     class Meta:
         model = UserInteraction
         fields = '__all__'
