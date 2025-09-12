7673eb949817b3e81e310572c2c519b6abcc6f3f
ramzi <ramzi.benmansour@artdigit.fr>
2025-09-12 20:53:14 +0200

    feat(api): add custom endpoint to fetch liked items by user id

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 
 from catalog.models import Category, Item, UserInteraction
 from catalog.serializers import CategorySerializer, ItemSerializer, UserInteractionSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -16,3 +18,11 @@ class ItemViewSet(viewsets.ModelViewSet):
 class UserInteractionViewSet(viewsets.ModelViewSet):
     queryset = UserInteraction.objects.all()
     serializer_class = UserInteractionSerializer
+
+    @action(detail=False, methods=['get'], url_path='liked-items/(?P<user_id>[^/.]+)')
+    def liked_items(self, request, user_id:None):
+        """Return all liked items for a specific user."""
+        liked_interactions = UserInteraction.objects.filter(user_id=user_id, liked=True)
+        items = [interaction.item for interaction in liked_interactions]
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
